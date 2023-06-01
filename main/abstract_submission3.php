8dbefb9a7705bbb6a5e408df8310d7c5f640579d
crystal <newcrystal670@gmail.com>
2023-06-01 16:55:35 +0900

    Update abstract_submission3.php

--- main/abstract_submission3.php
+++ main/abstract_submission3.php
@@ -477,8 +477,8 @@ $('.submit_btn').click(function() {
         success: function(res) {
             //console.log(res);
             if (res.code == 200) {
-		location.href = './mypage_abstract.php';
                 abstract_gmail(res.email, res.name, res.subject, res.title, res.topic_text);
+		location.href = './mypage_abstract.php';
                 //alert(locale(language.value)("send_mail_success"));
               
             }
